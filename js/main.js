6ffda541e6b9d35f4933cd1f6a9cc24a1de1f5fe
pavelsalfetkin <pavelsalf@yandex.ru>
2018-12-04 00:09:41 +0300

    pre finish commit

--- js/main.js
+++ js/main.js
@@ -1,24 +1,20 @@
-// console.log("main.js is ok");
-
-
 /* ---------------------------------------------------------------------------- */
 /* ---------------------------------  CONST  ---------------------------------- */
 /* ---------------------------------------------------------------------------- */
 
 
-var slider = document.getElementById("slider");
+var sliderCont = document.getElementById("slider-cont");
+var sliderIce = document.getElementById("slider");
 var slides = document.getElementById("slides");
-var sliderRunner = document.getElementById("runner");
 var section1 = document.getElementById("section1");
 var section2 = document.getElementById("section2");
 var section3 = document.getElementById("section3");
+var sliderRunner = document.getElementById("runner");
 var rightDots = document.getElementById("right-dots");
 var bottomBlock = document.getElementById("bottom-block");
 
 var circleAnimRadius1 = Array.from(document.getElementsByClassName("circle__radius-1"));
 var circleAnimRadius2 = Array.from(document.getElementsByClassName("circle__radius-2"));
-
-const runnerWidth = sliderRunner.offsetWidth / 100;
 
 
 /* ---------------------------------------------------------------------------- */
@@ -26,12 +22,13 @@ const runnerWidth = sliderRunner.offsetWidth / 100;
 /* ---------------------------------------------------------------------------- */
 
 
-function animation(circles, ms) {
+const RUNNER_WIDTH = sliderRunner.offsetWidth / 100;
 
+const animation = (circles) => {
 	var posStart = 4;
 	var posEnd = 13.4;
 	var shiftInterval = 0.2;
-
+	var ms = 40;
 	setInterval(frame, ms);
   function frame() {
     if (posStart >= posEnd) {
@@ -43,128 +40,9 @@ function animation(circles, ms) {
 			circles.forEach(x => x.r.baseVal.value = posStart);
     }
   }
-}
+};
 
-animation(circleAnimRadius1, 40);
-setTimeout(function() { animation(circleAnimRadius2, 40) }, 1000);
-bottomBlock.setAttribute('class', 'bottom-block-active');
-
-// window.addEventListener('wheel', function(e) {
-// 	console.log("move down", e.target);
-// });
-
-
-/* ---------------------------------------------------------------------------- */
-/* ---------------------------------  EVENTS  --------------------------------- */
-/* ---------------------------------------------------------------------------- */
-
-
-section1.addEventListener('touchstart', function(e) {
-	const startPoint = e.targetTouches[0].clientY;
-	let endPoint;
-	this.addEventListener('touchmove', function(e) { endPoint = e.targetTouches[0].clientY });
-	this.addEventListener('touchend', function(e) {
-		if (startPoint > endPoint) {
-			console.log("slide UP");
-			section2.scrollIntoView({block: "start", behavior: "smooth"});
-			rightDots.children[0].classList.remove('active');
-			rightDots.children[1].classList.add('active');
-			rightDots.children[2].classList.remove('active');
-		} else {
-			console.log("slide DOWN");
-		}
-	});
-});
-
-section2.addEventListener('touchstart', function(e) {
-	const startPoint = e.targetTouches[0].clientY;
-	let endPoint;
-	this.addEventListener('touchmove', function(e) { endPoint = e.targetTouches[0].clientY });
-	this.addEventListener('touchend', function(e) {
-		if (startPoint > endPoint) {
-			console.log("slide UP");
-			section3.scrollIntoView({block: "start", behavior: "smooth"});
-			bottomBlock.setAttribute('class', 'bottom-block-none');
-			rightDots.children[0].classList.remove('active');
-			rightDots.children[1].classList.remove('active');
-			rightDots.children[2].classList.add('active');
-		} else {
-			console.log("slide DOWN");
-			section1.scrollIntoView({block: "start", behavior: "smooth"});
-			bottomBlock.setAttribute('class', 'bottom-block-active');
-			rightDots.children[0].classList.add('active');
-			rightDots.children[1].classList.remove('active');
-			rightDots.children[2].classList.remove('active');
-		}
-	});
-});
-
-section3.addEventListener('touchstart', function(e) {
-	const startPoint = e.targetTouches[0].clientY;
-	let endPoint;
-	this.addEventListener('touchmove', function(e) { endPoint = e.targetTouches[0].clientY });
-	this.addEventListener('touchend', function(e) {
-		if (startPoint > endPoint) {
-			console.log("slide UP");
-		} else {
-			console.log("slide DOWN");
-			section2.scrollIntoView({block: "start", behavior: "smooth"});
-			rightDots.children[0].classList.remove('active');
-			rightDots.children[1].classList.add('active');
-			rightDots.children[2].classList.remove('active');
-		}
-	});
-});
-
-
-
-
-
-section1.addEventListener('wheel', function(e) {
-	// console.log("this", this);
-	if (e.deltaY > 10) {
-		// console.log("move down", e.deltaY);
-		section2.scrollIntoView({block: "start", behavior: "smooth"});
-		rightDots.children[0].classList.remove('active');
-		rightDots.children[1].classList.add('active');
-		rightDots.children[2].classList.remove('active');
-	};
-});
-
-section2.addEventListener('wheel', function(e) {
-	// console.log("this", this);
-	if (e.deltaY > 10) {
-		// console.log("move down", e.deltaY);
-		section3.scrollIntoView({block: "start", behavior: "smooth"});
-		bottomBlock.setAttribute('class', 'bottom-block-none');
-		// bottomBlock.classList.add('none');
-		rightDots.children[0].classList.remove('active');
-		rightDots.children[1].classList.remove('active');
-		rightDots.children[2].classList.add('active');
-	}
-	if (e.deltaY < -10) {
-		// console.log("move up", e.deltaY);
-		section1.scrollIntoView({block: "start", behavior: "smooth"});
-		bottomBlock.setAttribute('class', 'bottom-block-active');
-		rightDots.children[0].classList.add('active');
-		rightDots.children[1].classList.remove('active');
-		rightDots.children[2].classList.remove('active');
-	}
-});
-
-section3.addEventListener('wheel', function(e) {
-	// console.log("this", this);
-	if (e.deltaY < -10) {
-		// console.log("move up", e.deltaY);
-		section2.scrollIntoView({block: "start", behavior: "smooth"});
-		rightDots.children[0].classList.remove('active');
-		rightDots.children[1].classList.add('active');
-		rightDots.children[2].classList.remove('active');
-	};
-});
-
-slider.addEventListener('input', function() {
-	// console.log(this.value);
+function bottomSlider () {
 	if (this.value <= 100 && this.value >= 71) {
 		slides.style.right = "0";
 	}
@@ -174,6 +52,126 @@ slider.addEventListener('input', function() {
 	if (this.value <= 30 && this.value >= 1) {
 		slides.style.right = "-200%";
 	}
-	sliderRunner.style.width = runnerWidth * this.value + "px";
-	// console.log(Math.round(runnerWidth * this.value) + "px");
+	sliderRunner.style.width = RUNNER_WIDTH * this.value + "px";
+};
+
+const switchToSection1 = () => {
+	console.log("slide to section1");
+	section1.scrollIntoView({block: "start", behavior: "smooth"});
+	bottomBlock.classList.remove('none');
+	bottomBlock.classList.add('active');
+	rightDots.children[0].classList.add('active');
+	rightDots.children[1].classList.remove('active');
+	rightDots.children[2].classList.remove('active');
+};
+
+const switchToSection2 = () => {
+		console.log("slide to section2");
+		section2.scrollIntoView({block: "start", behavior: "smooth"});
+		bottomBlock.classList.add('none');
+		bottomBlock.classList.remove('active');
+		rightDots.children[0].classList.remove('active');
+		rightDots.children[1].classList.add('active');
+		rightDots.children[2].classList.remove('active');
+};
+
+const switchToSection3 = () => {
+		console.log("slide to section3");
+		section3.scrollIntoView({block: "start", behavior: "smooth"});
+		// bottomBlock.setAttribute('class', 'bottom-block-none');
+		rightDots.children[0].classList.remove('active');
+		rightDots.children[1].classList.remove('active');
+		rightDots.children[2].classList.add('active');
+};
+
+function swipeFromSection1 (event) {
+	this.startPoint = event.targetTouches[0].clientY;
+	this.endPoint;
+	this.addEventListener('touchmove', function(event) {
+		this.endPoint = event.targetTouches[0].clientY;
+	});
+	this.addEventListener('touchend', function() {
+		if (this.startPoint > this.endPoint) switchToSection2();
+	});
+}
+
+function swipeFromSection2 (event) {
+	this.startPoint = event.targetTouches[0].clientY;
+	this.endPoint;
+	this.addEventListener('touchmove', function(event) {
+		this.endPoint = event.targetTouches[0].clientY;
+	});
+	this.addEventListener('touchend', function() {
+		if (this.startPoint > this.endPoint) switchToSection3();
+		else switchToSection1();
+	});
+	
+}
+
+function swipeFromSection3 (event) {
+	this.startPoint = event.targetTouches[0].clientY;
+	this.endPoint;
+	this.addEventListener('touchmove', function(event) {
+		this.endPoint = event.targetTouches[0].clientY;
+	});
+	this.addEventListener('touchend', function() {
+		if (this.startPoint < this.endPoint) switchToSection2();
+	});
+}
+
+// const touchEventRemove = () => {
+// 	console.log("section - remove")
+// 	section1.removeEventListener('touchstart', swipeFromSection1);
+// 	section2.removeEventListener('touchstart', swipeFromSection3);
+// 	section3.removeEventListener('touchstart', swipeFromSection3);
+// };
+
+// const touchEventAdd = () => {
+// 	setTimeout(function() {
+// 		console.log("section - add")
+// 		section1.addEventListener('touchstart', swipeFromSection1);
+// 		section2.addEventListener('touchstart', swipeFromSection2);
+// 		section3.addEventListener('touchstart', swipeFromSection3);
+// 	}, 10000);
+// };
+
+
+/* ---------------------------------------------------------------------------- */
+/* ---------------------------------  EVENTS  --------------------------------- */
+/* ---------------------------------------------------------------------------- */
+
+
+section1.addEventListener('touchstart', swipeFromSection1);
+section2.addEventListener('touchstart', swipeFromSection2);
+section3.addEventListener('touchstart', swipeFromSection3);
+
+section1.addEventListener('wheel', function(e) {
+	if (e.deltaY > 10) switchToSection2();
 });
+
+section2.addEventListener('wheel', function(e) {
+	if (e.deltaY < -10) switchToSection1();
+	else if (e.deltaY > 10) switchToSection3();
+});
+
+section3.addEventListener('wheel', function(e) {
+	if (e.deltaY < -10) switchToSection2();
+});
+
+// sliderIce.addEventListener('touchstart', touchEventRemove);
+// sliderIce.addEventListener('touchend', touchEventAdd);
+
+sliderIce.addEventListener('input', bottomSlider);
+
+// screen.lockOrientation('landscape');
+
+
+/* ---------------------------------------------------------------------------- */
+/* ----------------------------------  INIT  ---------------------------------- */
+/* ---------------------------------------------------------------------------- */
+
+
+(() => {
+	animation(circleAnimRadius1);
+	setTimeout(function() { animation(circleAnimRadius2) }, 1000);
+})();
